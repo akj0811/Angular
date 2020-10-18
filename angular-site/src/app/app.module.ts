96264e730309167c9cbf350cbeef3bab68abd447
Vibhav Aggarwal <vibhav200601@gmail.com>
2020-10-18 19:01:51 +0530

    Contact page added

--- angular-site/src/app/app.module.ts
+++ angular-site/src/app/app.module.ts
@@ -43,7 +43,7 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatInputModule,
     MatGridListModule,
     MatRadioModule,
-    MatSnackBarModule
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
